48c714bf53b7fb43a250f210ac9809eb84488f4d
Tuumix <wellz-tkn@hotmail.com>
2023-07-07 12:25:37 -0300

    Updates

---
+++ precache-manifest.504abf1c7f1fb19d324f4d323c84d640.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b42406dc1df547fb4540",
+    "url": "/takanodev/static/css/main.22769399.chunk.css"
+  },
+  {
+    "revision": "b42406dc1df547fb4540",
+    "url": "/takanodev/static/js/main.b42406dc.chunk.js"
+  },
+  {
+    "revision": "23e5026d3c124a934bc7",
+    "url": "/takanodev/static/js/1.23e5026d.chunk.js"
+  },
+  {
+    "revision": "3713be7c5cef5ecb7f9f",
+    "url": "/takanodev/static/js/2.3713be7c.chunk.js"
+  },
+  {
+    "revision": "1121ed327c44284c904a",
+    "url": "/takanodev/static/js/runtime~main.1121ed32.js"
+  },
+  {
+    "revision": "72697c67516322aee50bf98d11441913",
+    "url": "/takanodev/static/media/avatar.72697c67.jpg"
+  },
+  {
+    "revision": "bcfa4586356f1e9cc10ce7b7c8168b58",
+    "url": "/takanodev/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.856db2929386e8fbaf13713d101c6157.js"
+  "/takanodev/precache-manifest.504abf1c7f1fb19d324f4d323c84d640.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/takanodev/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
